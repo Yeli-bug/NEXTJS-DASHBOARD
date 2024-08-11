829666cc8fa0f8b06f2e38940685437316e7a2ad
Yeli <iley.yelian@gmail.com>
2024-08-11 00:48:32 -0600

    Primer commit

--- app/ui/invoices/status.tsx
+++ app/ui/invoices/status.tsx
@@ -1,11 +1,11 @@
 import { CheckIcon, ClockIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 
+//ESTILOS CON CONDICION CLSX  #4
 export default function InvoiceStatus({ status }: { status: string }) {
   return (
     <span
-      className={clsx(
-        'inline-flex items-center rounded-full px-2 py-1 text-xs',
+      className={clsx(        
         {
           'bg-gray-100 text-gray-500': status === 'pending',
           'bg-green-500 text-white': status === 'paid',
